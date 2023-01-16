05242a3332b8e93a79fe915acec54868e3ab5148
Bryn Williams <brynwilliams@Bryns-MacBook-Pro.local>
2023-01-16 19:12:53 +0000

    added scanner so will ask continuously until correct answer provided

--- pet.java
+++ pet.java
@@ -1,23 +1,44 @@
 import java.util.Scanner;
 
 public class pet {
+
     public static void main(String[] args) {
         String dontLikeFoodOne = "\uD83D\uDE21";
-        String faveFoodOne = "\uD83E\uDD17";
+        String dontLikeFoodTwo = "\uD83E\uDD22";
+        String dontLikeFoodThree = "\uD83E\uDD2E";
 
-        if (food().equals("beans") ) {
-            System.out.println(faveFoodOne);
-        } else {
-            System.out.println(dontLikeFoodOne);
-        }	
-    }
+        String faveFoodOne = "\uD83D\uDE42";
+        String faveFoodTwo = "\uD83E\uDD17";
+        String faveFoodThree = "\uD83D\uDE06";
 
-    public static String food() {
+        String food = "";
         System.out.println("I'm hungry");
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("What's my food: ");
-        String food = scanner.nextLine().trim();
-        scanner.close();
-        return food;
+        food = scan.next();
+
+        while (!food.equals("beans")) {
+            System.out.println(dontLikeFoodOne);
+            System.out.print("What's my food: ");
+            food = scan.next();
+                
+        }
+        if (food.equals("beans") ) {
+            scan.close();
+            System.out.println(faveFoodTwo);
+        }
     }
+
+
+    // public static String food() {
+    //     String food = "";
+    //     System.out.println("I'm hungry");
+    //     Scanner scan = new Scanner(System.in);
+    //     System.out.print("What's my food: ");
+    //     System.out.println("%%%%%%");
+    //     food = scan.next();
+    //     System.out.println("********");
+    //     // scan.close();
+    //     return food;
+    // }
 }
